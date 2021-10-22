163e8026866603411174b359278d9805e67deb97
Mark Loercher <markloercher@marks-MacBook-Pro.local>
2021-10-22 18:09:22 -0400

    BCS

--- ron.js
+++ ron.js
@@ -1,180 +1,99 @@
-// Assignment code here
-
 //Array of Uppercase characters to be included in PW
 var upperCaseCharacters = [
-    'A',
-    'B',
-    'C',
-    'D',
-    'E',
-    'F',
-    'G',
-    'H',
-    'I',
-    'J',
-    'K',
-    'L',
-    'M',
-    'N',
-    'O',
-    'P',
-    'Q',
-    'R',
-    'S',
-    'T',
-    'U',
-    'V',
-    'W',
-    'X',
-    'Y',
-    'Z'
-  ];
-  //Array of lowercase characters to be included in PW
-  var lowerCaseCharacters = [
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'h',
-    'i',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-    'q',
-    'r',
-    's',
-    't',
-    'u',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z'
-  ];
-  //Array of numeric characters to be included in PW
-  var numericCharacters = [
-    '0',
-    '1',
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
-    '7',
-    '8',
-    '9'
-  ];
-  //Array of special characters to be included in PW
-  var specialCharacters = [
-    '@',
-    '%',
-    '+',
-    '\\',
-    '/',
-    "'",
-    '!',
-    '#',
-    '$',
-    '^',
-    '?',
-    ':',
-    ',',
-    ')',
-    '(',
-    '}',
-    '{',
-    ']',
-    '[',
-    '~',
-    '-',
-    '_',
-    '.'
-  ];
-  var generateBtn = document.querySelector("#generate");
-  //var characters = prompt ("how many characters must your password be?");
-  
-  
-  // Gnerate password
-  function generatePassword(){
-  
-  
-  }
-  
-  function getUserOptions () {
-  
-    var userChoice = {};
-  
-    //check user options
-      var userChoices = getUserOptions();
-    
-      //ask for length
-      var length = prompt('how many characters must your password be? (between 8-128 for best security)')
-  
-      ///To-Do (later): validate length
-    
-      //sign to userOptions
-      userChoices.length = length;
-      
-  
-      //ask for upperCase
-      var upperCase = confirm("do you want to use Upper Case characters?")
-  
-      //sign to userOptions
-      userChoices.upperCase = upperCase;
-      
-  
-      //ask for lowerCase
-      var lowerCase = confirm('do you want to use lowercase characters?')
-  
-      //sign to userOptions
-      userChoices.lowerCase = lowerCase;
-  
-      //ask for aplhanumerica
-      var numeric = confirm('do you want to use numeric characters?')
-  
-      //sign to userOptions
-      userChoices.numeric = numeric;
-  
-      //ask for special characters
-      var special = confirm('do you want to use special characters?')
-  
-      //sign to userOptions
-  
-      userChoices.special = special;
-      return userChoices;
-  
-  
-      
-  
-  
-  
-  
-  
-    console.log(parseInt(length))
-  }
-  
-  
-  // Write password to the #password input
-  function writePassword() {
-  
-    //get user options
-    var userOptions = getUserOptions();
-    
-  
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-  
-    passwordText.value = password;
-  
-  }
-  
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
-  
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+//Array of lowercase characters to be included in PW
+var lowerCaseCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+//Array of numeric characters to be included in PW
+var numericCharacters = [
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9'
+];
+//Array of special characters to be included in PW
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+var generateBtn = document.querySelector("#generate");
+//var characters = prompt ("how many characters must your password be?");
--- script.js
+++ script.js
@@ -1,172 +1,129 @@
 // Assignment code here
-
 //Array of Uppercase characters to be included in PW
 var upperCaseCharacters = [
-    'A',
-    'B',
-    'C',
-    'D',
-    'E',
-    'F',
-    'G',
-    'H',
-    'I',
-    'J',
-    'K',
-    'L',
-    'M',
-    'N',
-    'O',
-    'P',
-    'Q',
-    'R',
-    'S',
-    'T',
-    'U',
-    'V',
-    'W',
-    'X',
-    'Y',
-    'Z'
-  ];
-  //Array of lowercase characters to be included in PW
-  var lowerCaseCharacters = [
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'h',
-    'i',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-    'q',
-    'r',
-    's',
-    't',
-    'u',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z'
-  ];
-  //Array of numeric characters to be included in PW
-  var numericCharacters = [
-    '0',
-    '1',
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
-    '7',
-    '8',
-    '9'
-  ];
-  //Array of special characters to be included in PW
-  var specialCharacters = [
-    '@',
-    '%',
-    '+',
-    '\\',
-    '/',
-    "'",
-    '!',
-    '#',
-    '$',
-    '^',
-    '?',
-    ':',
-    ',',
-    ')',
-    '(',
-    '}',
-    '{',
-    ']',
-    '[',
-    '~',
-    '-',
-    '_',
-    '.'
-  ];
-  var generateBtn = document.querySelector("#generate");
-  //var characters = prompt ("how many characters must your password be?");
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+//Array of lowercase characters to be included in PW
+var lowerCaseCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+//Array of numeric characters to be included in PW
+var numericCharacters = [
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9'
+];
+//Array of special characters to be included in PW
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+
+var pwLength = prompt("How many characters would you like your password to be? (Must be 8-128)")
+console.log(pwLength);
+
+while (prompt.pwlength >=8 && prompt.pwLength <=128){
+  window.confirm('Would you like to use Uppercase characters?');
+  window.confirm('Would you like to use lowercase characters?');
+  window.confirm('Would you like to use numbers?');
+  window.confirm('Would you like to use special characters?');
+};
+
+
   
-  
-  // Gnerate password
-  function generatePassword(){
-      
-    const totalLength = prompt('How many characters would you like to use?');
-    if(totalLength >= 8 && totalLength <= 128) {
-        alert(`Your new Password will be ${totalLength}
-        characters long`);
-    }  
-   // var options = {
-     //   hasNumber : window.confirm('Would you like to include numbers?'),
-       // hasUppercase : window.confirm('Would you like to include Upper case letters?'),
-        //hasLowercase : window.confirm('Would you like to include Lower case letters?'),
-        //has Symbol : window.confirm('Would you like to include Special characters?'),
-    //};
-  
-    //if(options.hasNumber === false && options.hasUppercase === false && options.hasLowercase === false && options.hasSymbol === false)
-
-    let upper = confirm("Would you like to include Upper case letters?");
-    let lower = confirm("Would you like to include Lower case letters?");
-    let symbol = confirm("Would you like to include Special characters?");
-    let number = confirm("Would you like to include numbers?");
-
-    let allChars = "";
-    let password = "";
-
-    if(upper) {
-        allChars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    }
-    if(lower) {
-        allChars += "abcdefghijklmnopqrstuvwxyz";
-    }
-    if (symbol) {
-        allChars += "@%+\\/'!#$^?:,)(}{][~-_."
-    }
-    if (number) {
-        allChars += "0123456789";
-    }
-
-    //generate pw macthing selected values 
-    //create a for loop to iterate selected values
-    //return the generated password
-
-    else {
-        alert('Please correctly answer the password criteria prompts :)')
-    }
 
 
 
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
 
 }
 
-
-  // Write password to the #password input
-  function writePassword() {
-  
-    //get user options
-    //var userOptions = getUserOptions();
-    
-  
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-  
-    passwordText.value = password;
-  
-  }
-  
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
-  
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
