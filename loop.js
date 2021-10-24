6e040a6d38a7b4f3d32fa0fec492ffad9c5c5baf
Mark Loercher <markloercher@marks-MacBook-Pro.local>
2021-10-24 17:11:25 -0400

    added guaranteedChars check

---
+++ loop.js
@@ -0,0 +1,28 @@
+console.log('Hello')
+
+
+// first: iterator initializer
+// second: conditional statement
+// third: iterator modifier --> takes effect at the end of every loop
+for(let i = 0;i < 5; i++) {
+    console.log('Hello world!')
+}
+
+// i = 0
+// if i < 5
+// 0 < 5 ---> true
+// 'Hello World!'
+// i++ ---> i = 0 + 1 ----> i = 1
+
+// i = 1
+// if i < 5
+// 1 < 5 --> true
+// 'Hello world!'
+// i++ ----> i = 2;
+
+// ...
+
+
+// i = 5
+// 5 < 5 ---> false
+
--- script.js
+++ script.js
@@ -1,257 +1,94 @@
 // Assignment code here
 var generateBtn = document.querySelector("#generate");
 
-//Array of Uppercase characters to be included in PW
-
-var upperCaseCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
-];
-//Array of lowercase characters to be included in PW
-var lowerCaseCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
-];
-//Array of numeric characters to be included in PW
-var numericCharacters = [
-  '0',
-  '1',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9'
-];
-//Array of special characters to be included in PW
-var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
-];
-/*var allChars = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z',
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z',
-  '0',
-  '1',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9',
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
-
-]*/
-
 // GENERATE PASSWORD FUNCTION!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 function generatePassword() {
+  // Getting User PW parameters
+  const pwLength = window.prompt(
+    "How many characters would you like your password to be?"
+  );
+let upper
+let lower
+let numeric
+let special
 
-// Getting User PW parameters
-const pwLength = window.prompt("How many characters would you like your password to be?");
-if(pwLength < 8 || pwLength > 128) {
-  alert('Please select a valid character length!');
-  }else {
-    var upper = confirm('Would you like to use Uppercase characters?');
-    var lower = confirm('Would you like to use lowercase characters?');
-    var numeric = confirm('Would you like to use numeric characters?');
-    var special = confirm('Would you like to use special characters?');
+const upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+const numericChars = "0123456789";
+const specialChars = "@%+\\/'!#$^?:,)(}{][~-_.";
+  if (pwLength < 8 || pwLength > 128) {
+    alert("Please select a valid character length!");
+  } else {
+     upper = confirm("Would you like to use Uppercase characters?");
+     lower = confirm("Would you like to use lowercase characters?");
+     numeric = confirm("Would you like to use numeric characters?");
+     special = confirm("Would you like to use special characters?");
   }
-  let allChars ='';
+  let allChars = "";
   
-if (upper) {
-  allChars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-}
-if (lower) {
-  allChars += "abcdefghijklmnopqrstuvwxyz";
-}
-if (numeric) {
-  allChars += "0123456789";
-}
-if (special) {
-  allChars += "@%+\\/'!#$^?:,)(}{][~-_.";
-}
+  let guaranteedChars = "";
+  
 
-if(upper === false && lower === false && numeric === false && special === false){
-  alert('Please select valid password parameters!');
-}
+  if (upper) {
+    guaranteedChars += upperCaseChars.charAt(Math.floor(Math.random() * upperCaseChars.length))
+     allChars += upperCaseChars;
+     // allChars = allChars + upperCaseChars;
+  }
+  if (lower) {
+    guaranteedChars += lowerCaseChars.charAt(Math.floor(Math.random() * lowerCaseChars.length))
+    allChars += lowerCaseChars;
+  }
+  if (numeric) {
+    guaranteedChars += numericChars.charAt(Math.floor(Math.random() * numericChars.length))
+    allChars += numericChars;
+  }
+  if (special) {
+    guaranteedChars += specialChars.charAt(Math.floor(Math.random() * specialChars.length))
+    allChars += specialChars;
+  }
+
+ console.log(guaranteedChars);
+  if (
+    upper === false &&
+    lower === false &&
+    numeric === false &&
+    special === false
+  ) {
+    alert("Please select valid password parameters!");
+  }
+
+  //console.log("allChars:", allChars);
+  var outputString = guaranteedChars;
 
 
-console.log('allChars:',allChars)
-var outputString ="";
-for (var i = 0; i < pwLength; i++) {
-outputString += allChars.charAt(Math.floor(Math.random() * allChars.length));
-console.log(outputString);
+  for (var i = 0; i < (pwLength - guaranteedChars.length); i++) {
+    outputString += allChars.charAt(
+      Math.floor(Math.random() * allChars.length))
+      // Math.floor(5.3) --> 5
+      // Math.floor(9.9) --> 9
+      // (0.67364526435 * 26) --> 17.51
+      // Mat.floor(17.51) ---> 17
+    console.log(outputString);
+  }
 
-}
-return outputString;
-
-//else 
 
 
-//Create for loop to iterate over user chosen characters
-//return the generated password
+
+
+  return outputString;
+
+  //else
+
+  //Create for loop to iterate over user chosen characters
+  //return the generated password
   //return password;
-
-
-
 }
 // END GENERATE PASSWORD FUNCTION!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
-
-
-
 
 /*if ALL values are true, generate random pw using allChars
 if (confirm.upper === true, confirm.lower === true, confirm.numeric === true, confirm.special === true){
   Math.floor(Math.random(allChars) * (1 + High - Low)) + Low;
 }*/
-
 
 // Write password to the #password input
 function writePassword() {
@@ -259,7 +96,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
